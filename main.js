9f6e366c90aa3c240f341b1070f77b5b3fd8efb0
yashashwinnigam <95994235+yashashwinnigam@users.noreply.github.com>
2022-05-16 19:46:02 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+function setup() {
+    canvas = createCanvas(280, 280);
+    canvas.center();
+    background("white");
+    canvas.mouseReleased(classifyCanvas);
+    synth = window.speechSynthesis;
+}
+
+function preload() {
+    classifier = ml5.imageClassifier('DoodleNet');
+}
+
+function clearCanvas() {
+
+    background("white")
+}
+
+function draw() {
+
+strokeWeight(13);
+stroke("black");
+if (mouseIsPressed) {
+    line(pmouseX, pmouseY, mouseX, mouseY);
+}
+}
+
+function classifyCanvas() {
+    classifier.classify(canvas, gotResult);
+}
+
+
+
+function gotResult(error, results) {
+    if (error) {
+        console.error(error);
+    }
+    console.log(results);
+    document.getElementById('label').innerHTML = 'Label: ' + results[0].label;
+
+    document.getElementById('confidence').innerHTMl = 'Confidence: ' + Math.round(results[0].confidence * 100) + '%';
+
+    utterThis = new SpeechSynthesisUtterance(results[0].label);
+    synth.speak(utterThis);
+}
